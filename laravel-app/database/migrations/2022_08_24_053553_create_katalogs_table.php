0917dc557fd56bf8a0dbec64113a11eeb5fd4869
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-08-24 13:36:33 +0800

    update Model || migration

---
+++ laravel-app/database/migrations/2022_08_24_053553_create_katalogs_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('katalogs', function (Blueprint $table) {
+            $table->id();
+            $table->string('nama_katalog', 20);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('katalogs');
+    }
+};
