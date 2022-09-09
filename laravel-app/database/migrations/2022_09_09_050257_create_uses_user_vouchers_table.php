a0a4d3ffa51e8e3af134fea81b1dfddc76ed9bc4
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-09 13:03:49 +0800

    Update Model

---
+++ laravel-app/database/migrations/2022_09_09_050257_create_uses_user_vouchers_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('uses_user_vouchers', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('uses_user_vouchers');
+    }
+};
