9109898000125c056aaf2689ec58165fa02d98a8
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-09 13:49:57 +0800

    Update Register

--- laravel-app/resources/views/auth/register.blade.php
+++ laravel-app/resources/views/auth/register.blade.php
@@ -1,15 +1,11 @@
 <x-guest-layout>
-    <section class="vh-100" style="background-color: #9A616D;">
+    <section class="" style="background-color: #9A616D;">
         <div class="container py-5 h-100">
             <div class="row d-flex justify-content-center align-items-center h-100">
-                <div class="col col-xl-10">
+                <div class="col col-md-6">
                     <div class="card" style="border-radius: 1rem;">
                         <div class="row g-0">
-                            <div class="col-md-6 col-lg-5 d-none d-md-block">
-                                <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/img1.webp"
-                                    alt="login form" class="img-fluid" style="border-radius: 1rem 0 0 1rem;" />
-                            </div>
-                            <div class="col-md-6 col-lg-7 d-flex align-items-center">
+                            <div class=" d-flex align-items-center">
                                 <div class="card-body p-4 p-lg-5 text-black">
                                     @if ($errors->any())
                                         <div {{ $attributes }}>
@@ -26,8 +22,7 @@
                                     <form action="{{ route('register') }}" method="POST">
                                         @csrf
                                         <div class="d-flex align-items-center mb-3 pb-1">
-                                            <i class="fas fa-cubes fa-2x me-3" style="color: #ff6219;"></i>
-                                            <span class="h1 fw-bold mb-0">Logo</span>
+                                          <x-jet-application-logo />
                                         </div>
                                         <div class="form-outline mb-4">
                                             <input type="text" name="name" id="name" :value="old('name')"
