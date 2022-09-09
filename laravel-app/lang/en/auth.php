79f16d0cb17dfc3da3455ea294e7e2ce7a95c3b4
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-09-09 10:29:06 +0800

    Update Auth

--- laravel-app/lang/en/auth.php
+++ laravel-app/lang/en/auth.php
@@ -13,8 +13,9 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Email Dan Password Tidak Cocok',
+    'password' => 'Password Tidak Cocok',
+    'throttle' => 'Terlalu Banyak Akses. Mohon Coba Kembali :seconds detik.',
+    'email'=> 'Email Tidak Terdaftar',
 
 ];
--- laravel-app/resources/views/auth/register.blade.php
+++ laravel-app/resources/views/auth/register.blade.php
@@ -14,7 +14,7 @@
                                     @if ($errors->any())
                                         <div {{ $attributes }}>
                                             <div class="font-medium text-red-600">
-                                                {{ __('Whoops! Something went wrong.') }}</div>
+                                                {{ __('Whoops! Terjadi Kesalahan.') }}</div>
 
                                             <ul class="mt-3 list-disc list-inside text-sm text-red-600">
                                                 @foreach ($errors->all() as $error)
