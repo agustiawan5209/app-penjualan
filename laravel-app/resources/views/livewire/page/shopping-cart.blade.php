ee5b6f479cdddec541092b8a728c5905602a10a4
thype5209 <44895587+thype5209@users.noreply.github.com>
2022-11-12 19:46:50 +0800

    Update ShoppingCart

--- laravel-app/resources/views/livewire/page/shopping-cart.blade.php
+++ laravel-app/resources/views/livewire/page/shopping-cart.blade.php
@@ -15,6 +15,13 @@
                             </div>
                         </div>
                         <span class="text-reddarken-700">Rp. {{ number_format($item->sub_total, 0, 2) }}</span>
+                        <button wire:click='removeCart({{ $item->id }})' class="px-1 py-2 text-red-500 text-sm font-semibold">
+                            <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16">
+                                </path>
+                            </svg>
+                        </button>
                     </div>
                 @endforeach
                 <hr class="mt-6 mb-4 bg-reddarken-300 ml-0">
