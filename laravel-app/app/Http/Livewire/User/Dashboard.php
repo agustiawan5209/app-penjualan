e96bf6bf7570b7caa4304731fb9a667c36db977b
Agustiawan <44895587+thype5209@users.noreply.github.com>
2022-11-24 20:47:43 +0800

    Update Dashboard user

--- laravel-app/app/Http/Livewire/User/Dashboard.php
+++ laravel-app/app/Http/Livewire/User/Dashboard.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Livewire\User;
 
+use App\Models\Keranjang;
+use App\Models\Pembayaran;
+use Auth;
 use Livewire\Component;
 
 class Dashboard extends Component
 {
     public function render()
     {
-        return view('livewire.user.dashboard')->layout('livewire.layout.customer');
+        $jumlah_pesanan = Pembayaran::where('user_id', Auth::user()->id)->where('payment_status', '=', '2')->sum('total_price');
+        $jumlah_pesanan_1 = Pembayaran::where('user_id', Auth::user()->id)->where('payment_status', '=', '1')->get();
+        $Jumlah_barang_keranjang = Keranjang::where('user_id', Auth::user()->id)->get();
+        return view('livewire.user.dashboard' ,[
+            'jumlah_pesanan'=> $jumlah_pesanan,
+            'Jumlah_barang_keranjang'=> $Jumlah_barang_keranjang,
+            'jumlah_pesanan_1'=> $jumlah_pesanan_1
+        ])->layout('livewire.layout.customer');
     }
 }
--- laravel-app/resources/views/livewire/user/dashboard.blade.php
+++ laravel-app/resources/views/livewire/user/dashboard.blade.php
@@ -6,27 +6,19 @@
                     <div class="flex flex-wrap">
                         <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
                             <h5 class="text-reddarken uppercase font-bold text-xs">
-                                Traffic
+                                Total Pembelian
                             </h5>
                             <span class="font-semibold text-xl text-reddarken">
-                                350,897
+                                Rp.{{ number_format($jumlah_pesanan,0,2) }}
                             </span>
                         </div>
                         <div class="relative w-auto pl-4 flex-initial">
                             <div
                                 class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-red-500">
-                                <i class="far fa-chart-bar"></i>
+                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
                             </div>
                         </div>
                     </div>
-                    <p class="text-sm text-white mt-4">
-                        <span class="text-emerald-500 mr-2">
-                            <i class="fas fa-arrow-up"></i> 3.48%
-                        </span>
-                        <span class="whitespace-nowrap">
-                            Since last month
-                        </span>
-                    </p>
                 </div>
             </div>
         </div>
@@ -36,25 +28,19 @@
                     <div class="flex flex-wrap">
                         <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
                             <h5 class="text-reddarken uppercase font-bold text-xs">
-                                New users
+                                Keranjang
                             </h5>
                             <span class="font-semibold text-xl text-reddarken">
-                                2,356
+                               {{$Jumlah_barang_keranjang->count()}}
                             </span>
                         </div>
                         <div class="relative w-auto pl-4 flex-initial">
                             <div
                                 class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-green-500">
-                                <i class="fas fa-chart-pie"></i>
+                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"></path></svg>
                             </div>
                         </div>
                     </div>
-                    <p class="text-sm text-white mt-4">
-                        <span class="text-red-500 mr-2">
-                            <i class="fas fa-arrow-down "></i> 3.48%
-                        </span>
-                        <span class="whitespace-nowrap"> Since last week </span>
-                    </p>
                 </div>
             </div>
         </div>
@@ -64,55 +50,19 @@
                     <div class="flex flex-wrap">
                         <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
                             <h5 class="text-reddarken uppercase font-bold text-xs">
-                                Sales
+                               Pembayaran Belum Di Konfirmasi
                             </h5>
                             <span class="font-semibold text-xl text-reddarken">
-                                924
+                                {{ $jumlah_pesanan_1->count() }}
                             </span>
                         </div>
                         <div class="relative w-auto pl-4 flex-initial">
                             <div
                                 class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-reddarken">
-                                <i class="fas fa-users"></i>
+                                <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"></path></svg>
                             </div>
                         </div>
                     </div>
-                    <p class="text-sm text-white mt-4">
-                        <span class="text-orange-500 mr-2">
-                            <i class="fas fa-arrow-down"></i> 1.10%
-                        </span>
-                        <span class="whitespace-nowrap"> Since yesterday </span>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="w-full lg:w-6/12 xl:w-3/12 px-4">
-            <div class="relative flex flex-col min-w-0 break-words bg-white rounded mb-6 xl:mb-0 shadow-lg">
-                <div class="flex-auto p-4">
-                    <div class="flex flex-wrap">
-                        <div class="relative w-full pr-4 max-w-full flex-grow flex-1">
-                            <h5 class="text-reddarken uppercase font-bold text-xs">
-                                Performance
-                            </h5>
-                            <span class="font-semibold text-xl text-reddarken">
-                                49,65%
-                            </span>
-                        </div>
-                        <div class="relative w-auto pl-4 flex-initial">
-                            <div
-                                class="text-reddarken p-3 text-center inline-flex items-center justify-center w-12 h-12 shadow-lg rounded-full bg-lightBlue-500">
-                                <i class="fas fa-percent"></i>
-                            </div>
-                        </div>
-                    </div>
-                    <p class="text-sm text-white mt-4">
-                        <span class="text-emerald-500 mr-2">
-                            <i class="fas fa-arrow-up"></i> 12%
-                        </span>
-                        <span class="whitespace-nowrap">
-                            Since last month
-                        </span>
-                    </p>
                 </div>
             </div>
         </div>
